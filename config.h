f2f6bc3ebfb78a69ad3ba8d487242a19e9a175d6
NotCreative21 <seanray410@gmail.com>
2022-01-27 20:55:06 -0500

    tweak colors

--- config.h
+++ config.h
@@ -7,13 +7,13 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=8" };
-static const char dmenufont[]       = "monospace:size=8";
-static const char col_gray1[]       = "#282828";               /* bar color */
+static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:size=9", "FiraCode-Regular:pixelsize=9:antialias=true:autohint=true", "monospace:size=10" };
+static const char dmenufont[]       = "etBrainsMonoMedium Nerd Font:size=9";
+static const char col_gray1[]       = "#1d1f21";               /* bar color */
 static const char col_gray2[]       = "#a89984";
-static const char col_gray3[]       = "#ebdbb2";               /* bar font  */
-static const char col_gray4[]       = "#8ec07c";
-static const char col_cyan[]        = "#928374";               /* selected bar color*/
+static const char col_gray3[]       = "#707880";               /* bar font  */
+static const char col_gray4[]       = "#c5c8c6";
+static const char col_cyan[]        = "#1d1f21";               /* selected bar color*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "λ", "ξ", "Σ", "ζ", "δ", "θ", "μ", "Ψ", "Ω" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -33,6 +33,7 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
   	{ "st",       NULL,       NULL,       0,            1,           -1 },
   	{ "Steam",    NULL,       NULL,       0,            1,           -1 },
+	{ "Discord",  NULL,       NULL,       0,            1,           -1 },
 	{ "multimc",  NULL,       NULL,       0,            1,           -1 },
 };
 
@@ -63,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
